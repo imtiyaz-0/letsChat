0ee55bc38f9e42f4292894a55c708e742e0ef1eb
imtiyaz-0 <imti0615@gmail.com>
2024-01-14 13:01:15 +0530

    a

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors= require('cors');
+const mongoose = require('mongoose');
+
+
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+.connect(
+  process.env.MONGO_URL
+)
+.then(() => {
+  console.log("DB Connetion Successfull");
+})
+.catch((err) => {
+  console.log(err.message);
+});
+
+
+const server = app.listen(process.env.PORT ,()=>{
+    console.log(`server started at port no ${process.env.PORT}`);
+});
