3fbb5120c3bc193e6993a18553c3df8cfb8666dc
imtiyaz-0 <imti0615@gmail.com>
2024-01-15 17:59:02 +0530

    i

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -1,4 +1,27 @@
-module.exports.register=(req,res,next)=>{
+const User = require('../model/userModel');
+const brcypt = require('bcrypt');
 
-    console.log(req.body);
-};
+module.exports.register= async (req,res,next)=>{
+   try{ const {username , email, password } = req.body;
+    const usernameCheck = await User.findOne({username});
+
+    if(usernameCheck)
+    return res.json({msg:"Username already used " , status:false});
+
+    const  emailCheck = await User.findOne({email});
+
+    if(usernameCheck)
+    return res.json({msg:"Email already used " , status:false});
+
+    const hashedpassword = await brcypt.hash(password , 10);
+    const user = await User.create({
+        email ,
+        username, 
+       password: hashedpassword, 
+    });
+    delete user.password;
+    return res.json({status:true , user});
+    }catch(ex) {
+       next(ex);
+    }
+ };
