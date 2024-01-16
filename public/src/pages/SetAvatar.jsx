51401da1ff7b681a5cfb63fec7a558ef26858443
imtiyaz-0 <imti0615@gmail.com>
2024-01-16 12:40:49 +0530

    k

--- public/src/pages/SetAvatar.jsx
+++ public/src/pages/SetAvatar.jsx
@@ -6,7 +6,7 @@ import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
 import { setAvatarRoute } from '../utils/APIRoutes';
-
+import { Buffer } from 'buffer';
 
 function SetAvatar() {
     const api= "http://api.multiavatar.com/45678945";
@@ -24,24 +24,51 @@ function SetAvatar() {
       };
       const setProfilePicture = async()=>{};
 
-      useEffect(async ()=>{
-        const data = [];
-        for(let i=0;i<4;i++){
-            const image = await axios.get(
-                `${api}/${math.round(math.random()*1000)}`
-            );
-            const buffer = new Buffer(image.data)
-            data.push(buffer.toString("base64");
-        }
-        setAvatars(data);
-        setIsLoading(false);
-      },[])
-
+      useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const data = [];
+            for (let i = 0; i < 4; i++) {
+              const image = await axios.get(
+                `${api}/${Math.round(Math.random() * 1000)}`
+              );
+              const buffer = Buffer.from(image.data);
+              data.push(buffer.toString("base64"));
+            }
+            setAvatars(data);
+            setIsLoading(false);
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        };
+      
+        fetchData(); 
+      }, []);
+      
   return(
    <>
    <Container> 
     <div className="title-container">
         <h1>Pick an avatar as your profile picture</h1>
+    </div>
+    <div className="avatars">{
+        avatars.map((avatar,index)=>{
+            return(
+                <div 
+                key={index}
+                className  = {`avatar ${
+                    selectedAvatar===index? "selected":""
+                }`}
+                >
+                 <img
+                 src={`data:image/svg+xml;base64 , ${avatar}`}
+                 alt='avatar'
+                 onClick={()=>setSelectedAvatar(index)}
+                 />
+                 </div>
+            )
+        })
+    }
     </div>
      </Container>
   <ToastContainer/>
