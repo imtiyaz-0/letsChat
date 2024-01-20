38e0b8d52b4f0d9a2a10a5ab0380b011464347ff
imtiyaz-0 <imti0615@gmail.com>
2024-01-20 15:13:45 +0530

    final commit

--- public/src/App.js
+++ public/src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import {BrowserRouter,Routes , Route} from 'react-router-dom';
-import Register from './pages/Register';
-import Chat from './pages/Chat';
-import Login from './pages/Login';
-import 'react-toastify/dist/ReactToastify.css';
-import SetAvatar from './pages/SetAvatar';
-
-
-function App() {
-  return <BrowserRouter>
-  <Routes>
-   <Route path="/register" element={<Register/>} />
-   <Route path="/login" element={<Login/>} />
-   <Route path="/setAvatar" element={<SetAvatar/>} />
-   <Route path="/" element={<Chat/>} />
-
-
-  </Routes>
-  </BrowserRouter>
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SetAvatar from "./components/SetAvatar";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
-
